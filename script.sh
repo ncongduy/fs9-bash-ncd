ac0e02874e4443ad66dd760fa9ac3c3728222352
emilje <71398812+emilje@users.noreply.github.com>
2022-04-08 20:14:27 +0300

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-if [ $# == 0 ]
-then echo Please input at least one component that you wish to search for in your files.
-else
-for var in "$@"
-do
-count=$(grep -n -R "<$var" | wc -l)
-echo "$var - $count"
-done
-fi
