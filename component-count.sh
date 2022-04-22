4ed0cf7c4e59acd8ba9d0df010cfc7ad94f8ef4a
Duy <ncongduy@gmail.com>
2022-04-22 11:55:31 +0300

    feat: create a feature that user can input the directory

--- component-count.sh
+++ component-count.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-# Declare variable
-total_args=$#
-current_dir=$(pwd)
-src="$current_dir/src"
-
 # Check arguments
-[[ $total_args -le 0 ]] && echo "You should provide at least 1 argument." && exit 1
+total_args=$#
+
+[[ $total_args -le 0 ]] && echo "Please enter one or several names of components." && \
+  echo "Example 1: ./component-count.sh Button" && \
+  echo "Example 2: ./component-count.sh Button Header Footer" && \
+  exit 1
+
+# User input a path to React directory
+echo "Please enter a path to React directory."
+echo "For example: /home/frontend_project/country_app/src"
+read -p "Please enter a path here: " usr_path
+
+# Check user path exists
+while [[ ! -d $usr_path ]]; do
+  echo
+  echo "Directory ${usr_path} doese not exist."
+  echo "Please enter a path React directory again or press 'q' to exit."
+  read -p "Please enter a path here: " usr_path
+
+  [[ $usr_path == "q" || $usr_path == "Q"  ]] && exit 1
+done
+
+echo "Component count is below:"
+src=$usr_path
 
 # Count component
 function count_component() {
